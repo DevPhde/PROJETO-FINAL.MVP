b27b363f34f7fa56509e4523e7d767386ccfdf9c
Diego B <dbsmendes@gmail.com>
2023-03-15 17:14:10 -0300

    Feat: [userRecoveryPassword] -recovery password implemented with password generator and email sending

---
+++ server/src/userAuthentication/recoveryPassword/controller/recoveryPasswordController.js
@@ -0,0 +1,18 @@
+import { recoveryPasswordUseCase } from "../useCase/recoveryPasswordUseCase.js";
+import { Mailer } from "../../../provider/mailer/MailProvider.js";
+import { Response, ResponseError } from "../../../models/response/Response.js";
+
+export class recoveryPasswordController extends recoveryPasswordUseCase {
+
+    static mailer = new Mailer();
+
+    static recoveryPassword = async (req, res) => {
+        const data = req.body;
+        // const newPassword = await this.teste()
+        const newPassword = await this.verifyRecoveryPassword(data);
+        console.log(newPassword)
+        const sendNewPassword =  newPassword.status ? await this.mailer.sendMail("recoveryPassword", data, `sua nova senha é: ${newPassword.message}`) : res.status(409).send(newPassword);
+        console.log(sendNewPassword)
+        sendNewPassword ? res.status(200).send(new Response(true, "Nova senha enviada para o email cadastrado.")) : res.status(500).send(new ResponseError('Erro Crítico RPC 16L, contate o suporte.'))
+    }
+}
