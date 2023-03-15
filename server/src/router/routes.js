9802508a7ad0a1d67f1390d986553d7a1b5cdd9b
Diego B <dbsmendes@gmail.com>
2023-03-15 17:11:14 -0300

    Refactor: [half files] - modified the name of database accessors

--- server/src/router/routes.js
+++ server/src/router/routes.js
@@ -1,11 +1,13 @@
 import { Router } from "express";
 import { RegisterController } from "../userAuthentication/register/controller/registerController.js";
 import { AuthorizationController } from "../userAuthentication/authorization/controller/AuthorizationController.js";
-
+import { recoveryPasswordController } from "../userAuthentication/recoveryPassword/controller/recoveryPasswordController.js";
 export const router = Router();
 
 router
     .get('/', (res) => { res.status(200).send('CONNECTION OK')})
-    .post('/validationField', RegisterController.ValidateField)
+    .post('/validationfield', RegisterController.ValidateField)
     .post('/new/user', RegisterController.UserRegistration)
     .post('/user/authorization', AuthorizationController.VerifyUserAuthenticity)
+    .post('/user/recoverypassword', recoveryPasswordController.recoveryPassword)
+    // .post('/user/recoverypassword/resendmail', recoveryPasswordController.)
