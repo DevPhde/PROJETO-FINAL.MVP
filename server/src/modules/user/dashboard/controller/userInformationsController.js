7e5bf985974a616255675ef6a8fcf2425436eaee
Diego B <dbsmendes@gmail.com>
2023-03-19 03:52:10 -0300

    Feat: [total values] - implemented method to sum all values and return to front

--- server/src/modules/user/dashboard/controller/userInformationsController.js
+++ server/src/modules/user/dashboard/controller/userInformationsController.js
@@ -6,4 +6,16 @@ export class UserInformationsController extends UserInformationsUseCases {
         console.log(data)
         data.status ? res.status(200).send(data) : res.status(500).send(data)
     }
+
+    static totalValues = async (req, res) => {
+        const {authorization} = req.headers;
+        const data = await this.getTotalValues(authorization)
+        data.status ? res.status(200).send(data) : res.status(500).send(data);
+    }
+
+    static graphStatus = async (req, res) => {
+        const {authorization} = req.headers;
+        const data = await this.typeExpenseTotalValues(authorization)
+        data.status ? res.status(200).send(data) : res.status(500).send(data)
+    }
 }
