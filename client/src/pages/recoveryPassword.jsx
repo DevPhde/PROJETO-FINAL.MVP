6af48aeb6905e39973b7e0162fdbdd3a2ee13d1f
Diego B <dbsmendes@gmail.com>
2023-03-20 17:13:36 -0300

    Refactor: [recovery password Requisition] - Implemented message to user

--- client/src/pages/recoveryPassword.jsx
+++ client/src/pages/recoveryPassword.jsx
@@ -1,35 +1,52 @@
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import '../style/recoveryPassword.css'
 import ImgRecovery from '../images/img2.png'
 import { UserUseCases } from "../useCases/UserUseCases";
+import { Link } from "react-router-dom";
+import { VerticalModal } from "../components/Modal";
 
 function RecoveryPasswordPage() {
 
-  const [email, setEmail] = useState ([]);
+  const [email, setEmail] = useState('');
+  const [message, setMessage] = useState({
+    status: null,
+    message: ''
+  })
 
-  const recovery = () =>{
-    UserUseCases.recoveryUser(email)
-
+  const handleSubmit = async () => {
+    const data = await UserUseCases.recoveryUser(email)
+    setMessage(prevState => ({ ...prevState, status: data.status, message: data.message }))
+    if(data.status) {
+      setModalShow(true)
+    }
   }
-
+  const [modalShow, setModalShow] = useState(false);
   return (
     <main className="main-recovery">
+      <VerticalModal
+        show={modalShow}
+        onHide={() => setModalShow(false)}
+        title={'Recuperação de senha'}
+      to={'/'}
+        buttonName={'Fechar e ir para o Login'}
+        message={message.message}
+      />
       <div className="div-img-recovery">
-      <img className="img-recovery" src={ImgRecovery} />
+        <img className="img-recovery" src={ImgRecovery} />
       </div>
       <div className="div-info-recovery container text-center  align-items-center">
         <h1 className="fw-bold title-recovery mb-5">Esqueceu a senha?</h1>
 
         <p className=" text-recovery mb-5">Não se preocupe! Digite o seu email no campo abaixo, que nós enviaremos uma nova senha para você!</p>
-
         <div className="row mb-5 div-input-recovery">
-          <div >
+          <div>
             <input onChange={(e) => setEmail(e.target.value)} type="email" className="form-control input-recovery" id="colFormLabel" placeholder="Digite aqui o seu email" />
+            {!message.status && <p className="text-danger">{message.message}</p>}
           </div>
         </div>
         <div className="d-grid gap-2  div-btn-recovery">
-          <button className="btn btn-recovery fw-bold" type="button" onClick={recovery} >Enviar</button>
-          <button className="btn btn-recovery back fw-bold" type="button" onClick={recovery} >Voltar</button>
+          <button className="btn btn-recovery fw-bold" type="button" onClick={handleSubmit} >Enviar</button>
+          <button className="btn btn-recovery back fw-bold" type="button"><Link to="/" className="text-decoration-none text-white">Voltar</Link></button>
         </div>
 
       </div>
