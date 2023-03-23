34eaebb954d4316be7e3305e0c472bb1e958f7c2
Daniella <daniella.silqueiroz@gmail.com>
2023-03-22 21:25:56 -0300

    Update Login.jsx
    edit page login

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -5,37 +5,52 @@ import { UserUseCases } from "../useCases/UserUseCases";
 import { useNavigate } from "react-router-dom";
 
 function Login() {
-    
+
     const [email, setEmail] = useState();
     const [password, setPassword] = useState();
     const navigate = useNavigate();
-    async function Open() {
-        let re = /\S+@\S+\.\S+/;
-        if(re.test(email) == false || email == ""){
+    async function postLogin() {
+        let edit = /\S+@\S+\.\S+/;
+        if (edit.test(email) == false || email == "") {
             let err = document.getElementById("errEmail")
             err.style.display = "block";
 
         }
-        if(password == null){
+        if (password == null) {
             let err = document.getElementById("errPassword")
             err.style.display = "block";
         }
-        if(re.test(email) == true || email !== "" && password !== null){
-            const resp = await UserUseCases.Login(email, password);
-            console.log('rest funcionou',resp.status);
-            
-           if(resp.status == 200 ){
-            console.log("direcionando..")
-            navigate("/home");
-           }
-           if(resp.status == 401){
-            let err = document.getElementById("errInput2")
-            err.style.display = "block";
-            let errEmail = document.getElementById("errEmail")
-            errEmail.style.display = "none";
-            let errPassword = document.getElementById("errPassword")
-            errPassword.style.display = "none";
-           }
+        if (edit.test(email) == true && email !== "" && password !== undefined) {
+
+            try {
+                const resp = await UserUseCases.Login(email, password);
+                console.log('rest funcionou', resp.data.message);
+    
+                if (resp.status == 200) {
+                    console.log("direcionando..")
+                    const hash = resp.data.message;
+                    console.log('hash',hash)
+                    sessionStorage.setItem('authorization',hash)
+                    // navigate("/home");
+                }
+
+            } catch (e) {
+                 if (e.response.status == 500) {
+                    alert('Erro interno no servidor')
+                }
+                if (e.response.status == 401) {
+                    console.log('rest 401', e.response.status)
+                    let err = document.getElementById("errInput2")
+                    err.style.display = "block";
+                    let errEmail = document.getElementById("errEmail")
+                    errEmail.style.display = "none";
+                    let errPassword = document.getElementById("errPassword")
+                    errPassword.style.display = "none";
+                }
+                console.log('mensagem->',e)
+            }
+           
+
         }
     }
     return (
@@ -46,24 +61,30 @@ function Login() {
             <div className="div-info-recovery container text-center  align-items-center">
                 <h1 className="fw-bold title-recovery mb-5">Login</h1>
                 <div className="row mb-5 div-input-recovery">
-                <p className="errInput" id="errInput2">E-mail ou Senha inválidos</p>
+                    <p className="errInput" id="errInput2">E-mail ou Senha inválidos</p>
                     <div >
-                        <input type="email" className="form-control input-recovery"  placeholder="Digite aqui o seu E-mail" onChange={e => {
+                        <input type="email" className="form-control input-recovery" placeholder="Digite aqui o seu E-mail" onChange={e => {
                             setEmail(e.target.value);
                         }} />
-                       <p className="errEmail" id="errEmail">E-mail inválido</p>
+                        <p className="errEmail" id="errEmail">E-mail inválido</p>
                     </div>
                 </div>
                 <div className="row mb-5 div-input-recovery">
                     <div >
-                        <input type="password" className="form-control input-recovery"  placeholder="Digite aqui a sua Senha" onChange={e => {
+                        <input type="password" className="form-control input-recovery" placeholder="Digite aqui a sua Senha" onChange={e => {
                             setPassword(e.target.value);
                         }} />
-                       <p className="errPassword" id="errPassword">Senha inválida</p>
+                        <p className="errPassword" id="errPassword">Senha inválida</p>
                     </div>
                 </div>
+                <div className="form-check checkbox">
+                    <input className="form-check-input" type="checkbox" value="" id="flexCheckDefault" />
+                    <label className="form-check-label" htmlFor="flexCheckDefault">
+                        Lembra Senha
+                    </label>
+                </div>
                 <div className="d-grid gap-2  div-btn-recovery">
-                    <button className="btn btn-recovery fw-bold" type="button" onClick={Open}>Entrar</button>
+                    <button className="btn btn-recovery fw-bold" type="button" onClick={postLogin}>Entrar</button>
                 </div>
 
             </div>
