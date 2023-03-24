a45d1651f73dd4bc40ef49374d860cc08c33f7d4
Diego B <dbsmendes@gmail.com>
2023-03-24 15:28:35 -0300

    fix: [recoveryPassword] - Fixed bug where the backend tries to change the request header even after sending it.

--- server/src/modules/user/recoveryPassword/controller/recoveryPasswordController.js
+++ server/src/modules/user/recoveryPassword/controller/recoveryPasswordController.js
@@ -7,12 +7,20 @@ export class RecoveryPasswordController extends RecoveryPasswordUseCases {
     static mailer = new Mailer();
 
     static recoveryPassword = async (req, res) => {
-        const data = req.body;
-        const newPassword = await this.verifyRecoveryPassword(data);
-        const sendnewPassword =  newPassword.status ? await this.mailer.sendMail("recoveryPassword", data, `
-Foi solicitada uma recuperação de senha. Segue abaixo sua nova senha:
-
-Senha: ${newPassword.message}`) : res.status(401).send(newPassword);
-        sendnewPassword ? res.status(200).send(new Response(true, "Nova senha enviada para o email cadastrado.")) : res.status(500).send(new ResponseError('Erro Crítico RPC 16L, contate o suporte.'))
+        try {
+            const data = req.body;
+            const newPassword = await this.verifyRecoveryPassword(data);
+            if (!newPassword.status) {
+                return res.status(401).send(newPassword);
+            } else {
+                const sendNewPassword = await this.mailer.sendMail("recoveryPassword", data, `
+    Foi solicitada uma recuperação de senha. Segue abaixo sua nova senha:
+    
+    Senha: ${newPassword.message}`)
+                return sendNewPassword ? res.status(200).send(new Response(true, "Nova senha enviada para o email cadastrado.")) : res.status(500).send(new ResponseError('Erro Crítico RPC 21L, contate o suporte.'))
+            }
+        } catch (err) {
+            return res.status(500).send(new ResponseError('Erro Crítico RPC 22L, contate o suporte.'))
+        }
     }
 }
