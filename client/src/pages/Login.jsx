50c58c91aaa1eee379aa8ff2ce7e30d4ad2cd80f
Daniellasqw <daniella.silqueiroz@gmail.com>
2023-03-24 21:03:51 -0300

    Update Login.jsx

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -10,64 +10,48 @@ function Login() {
     const [password, setPassword] = useState();
     const navigate = useNavigate();
     useEffect(() => {
-
         let storageValue = localStorage.getItem('email');
-        console.log('email',storageValue)
         setEmail(storageValue);
-        
-      },[]);
+        if(sessionStorage.getItem('authorization')){
+            navigate("/home");
+        }
 
- 
+    }, []);
+    const [user, setUser] = useState(false)
+
     async function postLogin() {
         let check = document.getElementById("ipt1")
         if (check.checked) {
-            console.log("check ok")
-            localStorage.setItem('email',email)
+            localStorage.setItem('email', email)
         }
         let edit = /\S+@\S+\.\S+/;
         if (edit.test(email) == false || email == "") {
-            let err = document.getElementById("errEmail")
-            err.style.display = "block";
+            setUser(true)
 
         }
         if (password == null) {
-            let err = document.getElementById("errPassword")
-            err.style.display = "block";
+            setUser(true)
         }
         if (edit.test(email) == true && email !== "" && password !== undefined) {
 
             try {
                 const resp = await UserUseCases.Login(email, password);
-                console.log('rest funcionou', resp.data.message);
-
                 if (resp.status == 200) {
-                    console.log("direcionando..")
                     const hash = resp.data.message;
-                    console.log('hash', hash)
                     sessionStorage.setItem('authorization', hash)
-                    navigate("/dashboard");
+                    navigate("/home");
                 }
-
             } catch (e) {
                 if (e.response.status == 500) {
                     alert('Erro interno no servidor')
                 }
                 if (e.response.status == 401) {
-                    console.log('rest 401', e.response.status)
-                    let err = document.getElementById("errInput2")
-                    err.style.display = "block";
-                    let errEmail = document.getElementById("errEmail")
-                    errEmail.style.display = "none";
-                    let errPassword = document.getElementById("errPassword")
-                    errPassword.style.display = "none";
+                   setUser(true);
                 }
-                console.log('mensagem->', e)
             }
-
-
         }
     }
-    
+
     return (
         <main className="main-recovery">
             <div className="div-img-recovery">
@@ -76,20 +60,21 @@ function Login() {
             <div className="div-info-recovery container text-center  align-items-center">
                 <h1 className="fw-bold title-recovery mb-5">Login</h1>
                 <div className="row mb-5 div-input-recovery">
-                    <p className="errInput" id="errInput2">E-mail ou Senha inválidos</p>
+                    {user && <p className="errInput">E-mail ou senha inválido.</p>}
+                    {/* <p className="errInput" id="errInput2">E-mail ou Senha inválidos</p> */}
                     <div >
                         <input type="email" className="form-control input-recovery" placeholder="Digite aqui o seu E-mail" value={email} onChange={e => {
                             setEmail(e.target.value);
-                        }} />
-                        <p className="errEmail" id="errEmail">E-mail inválido</p>
+                        }}  onFocus={() => {setUser(false)}}/>
+                       
                     </div>
                 </div>
                 <div className="row mb-5 div-input-recovery">
                     <div >
                         <input type="password" className="form-control input-recovery" placeholder="Digite aqui a sua Senha" onChange={e => {
                             setPassword(e.target.value);
-                        }} />
-                        <p className="errPassword" id="errPassword">Senha inválida</p>
+                        }} onFocus={() => {setUser(false)}}/>
+                       
                     </div>
                 </div>
                 <div className="form-check checkbox">
@@ -99,23 +84,21 @@ function Login() {
                     </label>
                 </div>
                 <Link
-                    className='text-decoration-none text-dark'
-                    to='https://mvp-backend-k5vq.onrender.com/registerUser'
+                    className='text-decoration-none text-blue'
+                    to='https://mvp-backend-k5vq.onrender.com/recovery'
                 >Esqueceu a Senha?
                 </Link>
                 <div className="d-grid gap-2  div-btn-recovery">
                     <button className="btn btn-recovery fw-bold" type="button" onClick={postLogin}>Entrar</button>
                     <Link
-                        className='text-decoration-none text-dark'
+                        className='text-decoration-none text-blue'
                         to='https://mvp-backend-k5vq.onrender.com/registerUser'
                     >Ainda não possui Cadastro?
                     </Link>
                 </div>
-
             </div>
 
         </main>
     )
 }
-
 export default Login
